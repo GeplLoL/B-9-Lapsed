787c8cfa0b75d961b638254b274c498644ad837c
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-02-15 14:38:02 +0200

    Add files via upload

---
+++ B-9 Lapsed/B_9_Lapsed.py
@@ -0,0 +1,19 @@
+﻿from module1 import *
+nimed=[]
+keskmised_hinnad=[]
+listi = mitulapsed(nimed, keskmised_hinnad)
+print(nimed, keskmised_hinnad)
+x=input("vali, mida teha tahad\n1 Sortida loendeid\n2Näita õpilast\n3Leidke keskmine hinnang\n4Leidke lapse keskmine hinne, sisestades tema nime\n4Lisa andmed")
+if x=="1":
+    sort= sortirovka(nimed, keskmised_hinnad)
+    print(nimed, keskmised_hinnad)
+elif x=="2":
+    sort= otli4nik(nimed, keskmised_hinnad)
+    print(sort)
+elif x=="3":
+    keskmine(nimed, keskmised_hinnad)
+elif x=="4":
+    found(nimed, keskmised_hinnad)
+elif x=="5":
+    Lisa_andmed(nimed, keskmised_hinnad)
+print(nimed, keskmised_hinnad)
---
+++ B-9 Lapsed/module1.py
@@ -0,0 +1,76 @@
+﻿from random import *
+def mitulapsed(n:list,kh:list):
+    """Kirjeldus....
+    :param list i: Lapse järjend
+    :param list p: Hinne järjend
+    :rtype: list, list
+    """
+    x=int(input("kui palju õpilasi?"))
+    for i in range(x):
+        nimi= input("Sisestage nimi: ")
+        n.append(nimi)
+        randomHinne=round(uniform(1,5),1)
+        kh.append(randomHinne)
+    for o in range(len(n)):
+        print(f"{n[o]} {kh[o]}") 
+    return n, kh
+def sortirovka(n:list,kh:list):
+    """Kirjeldus....
+    :param list i: Lapse järjend
+    :param list p: Hinne järjend
+    :rtype: list, list
+    """
+    for p in range(0,len(n)):
+        for o in range(0,len(n)):
+            if n[p]<n[0]:
+                abi=n[p]
+                n[p]=n[o]
+                n[o]=abi
+                abi=kh[p]
+                kh[p]=kh[o]
+                kh[o]=abi
+    return n, kh
+def otli4nik(n:list,kh:list):
+    """Kirjeldus....
+    :param list i: Lapse järjend
+    :param list p: Hinne järjend
+    :rtype: list, list
+    """
+    if n==5:
+        ind=kh.index(kh)
+        nimi=n[ind]
+    else:
+        nimi="ei ole"
+    return nimi, n
+def keskmine(n:list,kh:list):
+    """Kirjeldus....
+    :param list i: Lapse järjend
+    :param list p: Hinne järjend
+    :rtype: list, list
+    """
+    kesk=sum(kh)/len(kh)
+    print(f"Keskmine palk on {kesk}")
+def found(n:list,kh:list):
+    """Kirjeldus....
+    :param list i: Lapse järjend
+    :param list p: Hinne järjend
+    :rtype: list, list
+    """
+    x= input("Sisestage oma nimi: ")
+    if x in n:
+        s = n.index(x)
+        b = kh[s]
+        print(n[s], round(b,1))
+def Lisa_andmed(n:list,kh:list):
+    """Kirjeldus....
+    :param list i: Lapse järjend
+    :param list p: Hinne järjend
+    :rtype: list, list
+    """
+    n=int(input("Mitu Lapsed:"))
+    for j in range(n):
+        laps=input("Sisesta nimi: ")
+        hinne=int(input("Sisesta hinne: "))
+        n.append(laps)
+        kh.append(hinne)
+    return n,kh
